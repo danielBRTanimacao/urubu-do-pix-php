3d66fd9940962cbcbbe69cbe371acc3591e5f722
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-07-16 13:47:10 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/remedy.css">
+    <link rel="stylesheet" href="./assets/css/index.css">
+    <title>Index</title>
+</head>
+<body>
+    <h1>Index</h1>
+</body>
+</html>
