b90f4624330e748eaf2245c69f234f05e6202321
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-07-17 13:43:59 -0300

    initialized project creting urubu do pix

--- index.php
+++ index.php
@@ -5,9 +5,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./assets/css/remedy.css">
     <link rel="stylesheet" href="./assets/css/index.css">
-    <title>Index</title>
+    <title>Urubu do pix - Index</title>
 </head>
 <body>
-    <h1>Index</h1>
+    <header class="bg-primary">
+        <h1 class="center-txt h1-header">Urubu do pix</h1>
+        <nav class="header-nav">
+            <ul class="nav">
+                <li class="nav-item">
+                    <a href="./app/pages/deposit.php" class="nav-link">
+                        Depositar
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        Sacar
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        Conta
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        principal
+    </main>
 </body>
 </html>
