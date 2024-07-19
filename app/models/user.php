b1c5c95c30298c663999da82e716de74e62c4200
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-07-19 15:46:02 -0300

    new feature on user creating register and login

--- app/models/user.php
+++ app/models/user.php
@@ -1,17 +1,28 @@
 <?php
+    $db = new SQLite3('db.sqlite3');
+
+    // Criar a tabela de usuários se não existir
+    $db->exec("CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL,
+        money REAL NOT NULL,
+    )");
+
+    echo "Banco de dados e tabela criados com sucesso.";
     class User {
-        public $name;
+        public $username;
         public $password;
         public $money;
 
-        public function __construct($name, $password, $money) {
-            $this->name = $name;
+        public function __construct($username, $password, $money) {
+            $this->username = $username;
             $this->password = $password;
             $this->money = $money;
         }
 
         public function showInfos() {
-            return "{$this->name}, Senha: {$this->password}, Dinheiro: {$this->money}";
+            return "Seu nome: {$this->username}, Senha: {$this->password}, Dinheiro: {$this->money}";
         }
     }
 ?>
--- app/pages/account.php
+++ app/pages/account.php
@@ -6,9 +6,31 @@
     <link rel="stylesheet" href="../../assets/css/remedy.css">
     <link rel="stylesheet" href="../../assets/css/index.css">
     <link rel="shortcut icon" href="../../public/urubu-icon.svg" type="image/x-icon">
-    <title>Urubu do pix - $user</title>
+    <title>Urubu do pix - user</title>
 </head>
 <body>
+    <header class="bg-primary">
+        <h1 class="center-txt h1-header">
+            Sobre o usuario - <?php echo"user"?>
+            <a href="../../index.php">
+                <img width="60" src="../../assets/svgs/logo-pix.svg" alt="icone_do_pix">
+            </a>
+        </h1>
+        <nav class="header-nav">
+            <ul class="nav">
+                <li class="nav-item">
+                    <a href="../../app/pages/trading.php" class="nav-link">
+                        Trading
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="../../app/pages/game.php" class="nav-link">
+                        Jogar
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
     <main>
         usuario
     </main>
